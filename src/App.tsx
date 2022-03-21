017745841365c2e9541eee2420747cf29af94c86
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-03-21 14:27:34 +0300

    Created folder components and put List-currencies there, created folder Select and created Select file there, created service folder and put getAllCurrencies function there

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
-import { getAllCurrencies } from 'src/helpers/getAllCurrencies';
-import { ListCurrencies } from 'src/helpers/list-currencies/List-currencies';
+import { getAllCurrencies } from 'src/service/getAllCurrencies';
+import { ListCurrencies } from 'src/components/List-currencies/List-currencies';
 
 function App() {
-  const [allCurrencies, setAllCurrencies] = useState([])
+  const [allCurrencies, setAllCurrencies] = useState([''])
 
   useEffect(() => {
-    getAllCurrencies(setAllCurrencies)
+    const res = async () => {
+      const data = await getAllCurrencies()
+      setAllCurrencies(data)
+    }
+    res()
   }, [])
 
   return (
     <div className="App">
       <h1>Change money</h1>
-      {ListCurrencies(allCurrencies)}
+      <ListCurrencies allCurrencies={allCurrencies} />
     </div>
   );
 }
