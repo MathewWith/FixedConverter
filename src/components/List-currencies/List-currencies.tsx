03903852e643df1a8bf1102a8e53bd5cfb09b320
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-03-22 12:10:31 +0300

    Updated List-currencies and getAllCurrencies files

--- src/components/List-currencies/List-currencies.tsx
+++ src/components/List-currencies/List-currencies.tsx
@@ -1,13 +1,13 @@
 import {useState} from "react"
 import { Select } from "../Select/Select"
 
-export const ListCurrencies = (allCurrencies: string[]) => {
+export const ListCurrencies: any = ({allCurrencies}: {allCurrencies: string[]}) => {
 
     const [currencyLeft, setCurrencyLeft] = useState(allCurrencies[0])
     const [currencyRight, setCurrencyRight] = useState(allCurrencies[0])
 
     return (
-        <div className="listCurrencies">
+        <div className="list-currencies">
             <Select allCurrencies={allCurrencies} setCurrency={setCurrencyLeft}/>
             <Select allCurrencies={allCurrencies} setCurrency={setCurrencyRight}/>
         </div>
