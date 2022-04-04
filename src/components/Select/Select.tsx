6dbff3541945f2c14a99703adf07a0c542a95e75
Matthew Ovsyannikov <v.pg.98@mail.ru>
2022-04-04 12:57:19 +0300

    Not important commit

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,12 +1,11 @@
 
 export const Select = (props: any) => {
-    const {allCurrencies} = props;
-    const {setCurrency} = props;
+    const {allCurrencies, setCurrency} = props;
     return (
         <select onChange={(e) => setCurrency(e.target.value)}>
                 {
-                    allCurrencies.map((cur: string) => {
-                       return <option key={cur}>{cur}</option>
+                    allCurrencies.map((currency: string) => {
+                       return <option key={currency}>{currency}</option>
                     })
                 }
         </select>
